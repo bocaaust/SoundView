a1274e8751efcd2afa70c01b9b3c849ed341fecf
Austin Lubetkin <bocaaust@gmail.com>
2016-04-30 01:23:49 -0400

    Added Color Blindness Test

--- SoundView.codea/Main.lua
+++ SoundView.codea/Main.lua
@@ -1,11 +1,13 @@
 -- SoundView
-displayMode(FULLSCREEN)
+--displayMode(FULLSCREEN)
 
 -- Use this function to perform your initial setup
 function setup()
     position = 0
+    selector = 1
+    parameter.integer( "BackgroundImage", 1, 5, 1, redrawBG )
     sounds = {"Menu Select","violin1","violin2","violin3"}
-    imges = {"spectrum","monalisa","trumptrain","AustinArt1","AustinArt2","AustinArt3","AustinArt4"}
+    imges = {"spectrum","monalisa","trumptrain","test1","test2"}
     spriteMode(CORNER)
     img = image(WIDTH,HEIGHT)
     setContext(img)
@@ -20,16 +22,33 @@ function draw()
     
 end
 
-function orientationChanged(newOrientation)
+function redrawBG()
     img = image(WIDTH,HEIGHT)
     setContext(img)
-  
-    if imges ~= nil then
-    sprite("Project:"..imges[1],0,0,WIDTH,HEIGHT)
+
+    if imges ~= nil and selector ~= nil then
+        sprite("Project:"..imges[selector],0,0,WIDTH,HEIGHT)
     else
-    sprite("Project:spectrum",0,0,WIDTH,HEIGHT)
+        sprite("Project:spectrum",0,0,WIDTH,HEIGHT)
     end
     setContext()
+end
+
+function redrawBG(input)
+    selector = input
+    img = image(WIDTH,HEIGHT)
+    setContext(img)
+
+    if imges ~= nil and selector ~= nil then
+        sprite("Project:"..imges[selector],0,0,WIDTH,HEIGHT)
+    else
+        sprite("Project:spectrum",0,0,WIDTH,HEIGHT)
+    end
+    setContext()
+end
+
+function orientationChanged(newOrientation)
+    redrawBG()
 end
 
 function touched(touch)
@@ -37,7 +56,7 @@ function touched(touch)
     r,g,b = img:get(math.floor(touch.x),math.floor(touch.y))
         sound("Game Sounds One:"..sounds[2],.33*r/255.,.20)
         sound("Game Sounds One:"..sounds[3],.33*b/255.,.5)
-        sound("Game Sounds One:"..sounds[4],.33*g/255.,.8)
+        sound("Game Sounds One:"..sounds[4],.23*g/255.,.8)
     end
 end
 
